62b2cd1fdf91dc11f6378d1f24d1c0fbeeec09dd
Wang Zi-yi <zainking@foxmail.com>
2019-06-10 16:45:52 +0800

    Add bit operate (#1)
    Add bit operate (#1)

--- src/main/java/com/github/hcsp/calculation/Cat.java
+++ src/main/java/com/github/hcsp/calculation/Cat.java
@@ -18,6 +18,15 @@ public class Cat {
     public Cat(boolean cute, boolean fat, boolean white) {
         // 请在此处根据cute/fat/white三个参数
         // 使用位运算对properties进行设值
+        if (cute) {
+            properties += CUTE;
+        }
+        if (fat) {
+            properties += FAT;
+        }
+        if (white) {
+            properties += WHITE;
+        }
     }
 
     /**
@@ -27,6 +36,7 @@ public class Cat {
      */
     public boolean isCute() {
         // 请在此处使用位运算读取properties，得到猫是否萌的结果
+        return (properties >>  (int)getLog2(CUTE) & 1) != 0;
     }
 
     /**
@@ -36,6 +46,7 @@ public class Cat {
      */
     public boolean isFat() {
         // 请在此处使用位运算读取properties，得到猫是否胖的结果
+        return (properties >> (int)getLog2(FAT) & 1) != 0;
     }
 
     /**
@@ -45,6 +56,7 @@ public class Cat {
      */
     public boolean isWhite() {
         // 请在此处使用位运算读取properties，得到猫是否白的结果
+        return (properties >> (int)getLog2(WHITE) & 1) != 0;
     }
 
     public static void main(String[] args) {
@@ -52,5 +64,9 @@ public class Cat {
         System.out.println("这只猫萌吗：" + cat.isCute());
         System.out.println("这只猫胖吗：" + cat.isFat());
         System.out.println("这只猫白吗：" + cat.isWhite());
+    }
+
+    public double getLog2(double num) {
+        return Math.log(num) / Math.log(2);
     }
 }
