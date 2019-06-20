9ecc5a14847491e5c309e1881b6633c43e52da82
Bo Zhang <bo@gradle.com>
2019-06-20 14:08:45 +0800

    Add a test

--- src/test/java/com/github/hcsp/calculation/CatTest.java
+++ src/test/java/com/github/hcsp/calculation/CatTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class CatTest {
     @Test
-    public void test() throws Exception {
+    public void test1() throws Exception {
         Cat cat = new Cat();
         cat.setCute(true);
         cat.setCute(true);
@@ -20,8 +20,12 @@ public class CatTest {
         Field field = Cat.class.getDeclaredField("properties");
         field.setAccessible(true);
         Assertions.assertEquals(0x1 | 0x4, field.get(cat));
+    }
 
-        cat = new Cat();
+    @Test
+    public void test2() throws Exception {
+
+        Cat cat = new Cat();
         for (int i = 0; i < 100; ++i) {
             cat.setCute(true);
             cat.setFat(true);
@@ -31,9 +35,14 @@ public class CatTest {
         Assertions.assertTrue(cat.isFat());
         Assertions.assertTrue(cat.isWhite());
 
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
         Assertions.assertEquals(0x1 | 0x2 | 0x4, field.get(cat));
+    }
 
-        cat = new Cat();
+    @Test
+    public void test3() throws Exception {
+        Cat cat = new Cat();
         for (int i = 0; i < 100; ++i) {
             cat.setCute(true);
             cat.setFat(true);
@@ -47,6 +56,24 @@ public class CatTest {
         Assertions.assertFalse(cat.isFat());
         Assertions.assertFalse(cat.isWhite());
 
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
         Assertions.assertEquals(0, field.get(cat));
+    }
+
+    @Test
+    public void test4() throws Exception {
+        Cat cat = new Cat();
+        cat.setFat(true);
+        cat.setWhite(true);
+        cat.setCute(false);
+
+        Assertions.assertTrue(cat.isFat());
+        Assertions.assertFalse(cat.isCute());
+        Assertions.assertFalse(cat.isWhite());
+
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
+        Assertions.assertEquals(0x2 | 0x4, field.get(cat));
     }
 }
