41511e2acb86bcd04ea104d258752a9b71120df7
squall1744 <30652145+squall1744@users.noreply.github.com>
2019-06-25 00:25:24 +0800

    bit-operation-setter-getter (#28)
    bit-operation-setter-getter (#28)

--- src/main/java/com/github/hcsp/calculation/Cat.java
+++ src/main/java/com/github/hcsp/calculation/Cat.java
@@ -14,42 +14,54 @@ public class Cat {
      *
      * @param cute true为萌，false为不萌
      */
-    public void setCute(boolean cute) {}
+    public void setCute(boolean cute) {
+        properties = cute ? (properties | CUTE) : (properties & (~CUTE));
+    }
 
     /**
      * 这只猫萌吗？请在此处使用位运算读取properties，得到猫是否萌的结果
      *
      * @return 萌则返回true，否则返回false
      */
-    public boolean isCute() {}
+    public boolean isCute() {
+        return (properties & CUTE) != 0;
+    }
 
     /**
      * 使用位运算设置猫咪胖的属性
      *
      * @param fat true为胖，false为不胖
      */
-    public void setFat(boolean fat) {}
+    public void setFat(boolean fat) {
+        properties = fat ? (properties | FAT) : (properties & (~FAT));
+    }
 
     /**
      * 这只猫胖吗？请在此处使用位运算读取properties，得到猫是否胖的结果
      *
      * @return 胖则返回true，否则返回false
      */
-    public boolean isFat() {}
+    public boolean isFat() {
+        return (properties & FAT) != 0;
+    }
 
     /**
      * 使用位运算设置猫咪白的属性
      *
      * @param white true为白，false为不白
      */
-    public void setWhite(boolean white) {}
+    public void setWhite(boolean white) {
+        properties = white ? (properties | WHITE) : (properties & (~WHITE));
+    }
 
     /**
      * 这只猫白吗？请在此处使用位运算读取properties，得到猫是否白的结果
      *
      * @return 白则返回true，否则返回false
      */
-    public boolean isWhite() {}
+    public boolean isWhite() {
+        return (properties & WHITE) != 0;
+    }
 
     public static void main(String[] args) {
         Cat cat = new Cat();
