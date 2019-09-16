f3ab65222064fa3e8abd7d182f8fdbd03f5bd615
zxc <guilongzhongwei@hotmail.com>
2019-09-16 16:15:54 +0800

    完善逻辑

--- src/main/java/com/github/hcsp/calculation/Cat.java
+++ src/main/java/com/github/hcsp/calculation/Cat.java
@@ -17,6 +17,8 @@ public class Cat {
     public void setCute(boolean cute) {
         if (cute) {
             properties |= CUTE;
+        } else {
+            properties = (~CUTE) & properties;
         }
     }
 
@@ -37,6 +39,8 @@ public class Cat {
     public void setFat(boolean fat) {
         if (fat) {
             properties |= FAT;
+        } else {
+            properties = (~FAT) & properties;
         }
     }
 
@@ -57,6 +61,8 @@ public class Cat {
     public void setWhite(boolean white) {
         if (white) {
             properties |= WHITE;
+        } else {
+            properties = (~WHITE) & properties;
         }
     }
 
