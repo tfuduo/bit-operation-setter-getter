5ab7611d81ab3bd420fe28b492091ef01cf37e8b
xuanzebin <xuanzebin@126.com>
2019-10-03 14:51:38 +0800

    fix: 修复了两次setter的情况

--- src/main/java/com/github/hcsp/calculation/Cat.java
+++ src/main/java/com/github/hcsp/calculation/Cat.java
@@ -15,6 +15,7 @@ public class Cat {
      * @param cute true为萌，false为不萌
      */
     public void setCute(boolean cute) {
+        if ( !cute && (properties & CUTE) != 0 ) properties ^= CUTE;
         if (cute) properties |= CUTE;
     }
 
@@ -33,6 +34,7 @@ public class Cat {
      * @param fat true为胖，false为不胖
      */
     public void setFat(boolean fat) {
+        if ( !fat && (properties & FAT) != 0 ) properties ^= FAT;
         if (fat) properties |= FAT;
     }
 
@@ -51,6 +53,7 @@ public class Cat {
      * @param white true为白，false为不白
      */
     public void setWhite(boolean white) {
+        if ( !white && (properties & WHITE) != 0 ) properties ^= WHITE;
         if (white) properties |= WHITE;
     }
 
